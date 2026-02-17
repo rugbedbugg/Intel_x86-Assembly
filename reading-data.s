c9c245e785307a079b33566118bb67647cd14f62
Arkixx <yes.par781@gmail.com>
2026-02-17 13:24:04 +0530

    [Syscalls]: I/O & Read operations

---
+++ reading-data.s
@@ -0,0 +1,18 @@
+.intel_syntax noprefix
+.global _start
+_start:
+	mov rdi, 0
+	mov rsi, 1337000
+	mov rdx, 8
+	mov rax, 0
+	syscall
+
+	mov rdi, 1
+	mov rsi, 1337000
+	mov rdx, 8
+	mov rax, 1
+	syscall
+
+	mov rdi, 42
+	mov rax, 60
+	syscall
---
+++ writing-output.s
@@ -0,0 +1,9 @@
+.intel_syntax noprefix
+.global _start
+_start:
+	mov rdi, 1
+	mov rsi, 1337000
+	mov rdx, 1
+
+	mov rax, 1
+	syscall
